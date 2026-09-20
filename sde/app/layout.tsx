f1ca2dcf46bcbe90fab3fcc7a0f255fe75a733b1
Ashwin Kumar Singh <kumarsinghashwin2007@gmail.com>
2026-09-21 00:55:03 +0530

    Create DayFlow SDE tracker web app

---
+++ sde/app/layout.tsx
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"DayFlow DSA",description:"A lightweight adaptive SDE problem tracker"};
+export default function RootLayout({children}:{children:React.ReactNode}){return <html lang="en"><body>{children}</body></html>}
